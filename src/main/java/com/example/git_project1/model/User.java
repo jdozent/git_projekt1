e47f8fb175a2c76cf2305ce02087ce2f33be0319
DESKTOP-13J4M6T\micablue <micha.schirmer@gmail.com>
2021-08-04 12:46:00 +0200

    Username hinzugefügt

--- src/main/java/com/example/git_project1/model/User.java
+++ src/main/java/com/example/git_project1/model/User.java
@@ -1,4 +1,18 @@
 package com.example.git_project1.model;
 
 public class User {
+
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
