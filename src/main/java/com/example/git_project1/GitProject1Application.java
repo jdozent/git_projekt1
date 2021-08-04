b914535dad76ed6f2b3287bc08a0af6f7df52d79
DESKTOP-13J4M6T\micablue <micha.schirmer@gmail.com>
2021-08-04 11:50:24 +0200

    init

---
+++ src/main/java/com/example/git_project1/GitProject1Application.java
@@ -0,0 +1,12 @@
+package com.example.git_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitProject1Application.class, args);
+    }
+}
---
+++ src/test/java/com/example/git_project1/GitProject1ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.git_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
